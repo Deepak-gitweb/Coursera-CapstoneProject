9d3374549771a2e188d051876c7b09b3e522335c
Deepak-gitweb <85065270+Deepak-gitweb@users.noreply.github.com>
2021-05-31 02:41:37 +0530

    Add files via upload

---
+++ Week-1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM DS Capstone Project Week-1
+
+# We will be working on the capstone project of IBM DS professional certificate, in this notebook.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
